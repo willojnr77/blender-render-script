51a12b5bd00cb870d3b6c4d495be856948624d66
Winston Wilkins <willo.jnr@gmail.com>
2015-01-20 07:27:33 -0500

    Fixed the next_frame reassignment statement in render_condition.sh

--- render_condition.sh
+++ render_condition.sh
@@ -16,8 +16,8 @@ for i in `ls $HOME/3D-Rot-me/*.blend`; do
       >> $HOME/log.txt'
     blender -b $HOME/3D-Rot-me/$j.blend -o $HOME/3D-Rot-$j/jpg/#.jpg -E CYCLES \
       -F JPG -s $next_frame -e $end_frame -a
-    next_frame=basename $(ls -1 $HOME/3D-Rot-$j/jpg | sort -g | tail -1) \
-      .jpg
+    next_frame=$(basename $(ls -1 $HOME/3D-Rot-$j/jpg | sort -g | tail -1) \
+      .jpg)
     ssh $director_server 'echo "$(date) Blender stopped..." \
       >> $HOME/log.txt'
     attempt=$((attempt+1))
