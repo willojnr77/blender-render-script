72878e8dcfae61c8969e648a6c8b579b89131671
Winston Wilkins <willo.jnr@gmail.com>
2015-01-22 10:10:32 -0500

    corrected the way we pass variables to the remote server.

--- render_condition.sh
+++ render_condition.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #replace with python. Script is over 20 lines.
-render_server=$HOSTNAME
 director_server=ubuntu@blender-director.us-central1-b.spartan-lacing-691
 next_frame=1
 end_frame=30
@@ -9,7 +8,7 @@ limit=2
 
 gcloud compute config-ssh
 
-for i in `ls $HOME/3D-Rot-me/*.blend`; do
+for i in $(ls $HOME/3D-Rot-me/*.blend); do
   j=$(basename $i .blend)
   while [ $next_frame -lt $end_frame ]; do
     ssh $director_server 'echo "$(date) Blender is running..." \
@@ -22,25 +21,23 @@ for i in `ls $HOME/3D-Rot-me/*.blend`; do
       >> $HOME/log.txt'
     attempt=$((attempt+1))
     if [ $attempt -gt $limit ]; then
-      ssh $director_server 'echo "$(date) Something is not right. \
-        Check .blend file" >> $HOME/log.txt'
-      ssh $director_server 'mv $HOME/being_rendered/$j.blend \
-        $HOME/not_rendered'
+      ssh $director_server "echo '$(date) Something is not right. \
+        Check .blend file' >> $HOME/log.txt"
+      ssh $director_server "mv $HOME/being_rendered/$j.blend \
+        $HOME/not_rendered"
       break
     elif [ $next_frame -ge $end_frame ]; then
-      ssh $director_server 'echo "$(date) Render successful." >> $HOME/log.txt'
+      ssh $director_server "echo \"$(date) Render successful.\" >> $HOME/log.txt"
       ssh $director_server 'mv $HOME/being_rendered $HOME/rendered'
-      ssh $director_server 'mkdir -p $HOME/rendered_images/3D-Rot-$j/jpg'
-      scp $HOME/3D-Rot-$j/jpg $director_server:rendered_images/3D-Rot-$j/jpg
+      ssh $director_server 'mkdir -p $HOME/rendered_images/'
+      scp -r $HOME/3D-Rot-$j $director_server:rendered_images
       break
     else
-      ssh $director_server 'echo "$(date) Render has stopped. Checking \
-        if things are OK. Attempt no. $attempt" >> $HOME/log.txt'
+      ssh $director_server "echo \"$(date) Render has stopped. Checking \
+        if things are OK. Attempt no. $attempt\" >> $HOME/log.txt"
     fi
   done
 done
 #shutdown sequence
-ssh $director_server 'echo "$(date) Shutting down $render_server." \
-  >> $HOME/log.txt'
-ssh $director_server 'gcloud compute instances delete $render_server'
-
+ssh $director_server "echo \"$(date) Shutting down $HOSTNAME .\" >> $HOME/log.txt"
+ssh $director_server "gcloud compute instances delete $HOSTNAME"
