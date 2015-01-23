300b43d360cae773aef60f7509285f85b5e252db
Winston Wilkins <willo.jnr@gmail.com>
2015-01-23 09:39:53 -0500

    Passed more variables from render_server to director_server

--- render_condition.sh
+++ render_condition.sh
@@ -11,14 +11,14 @@ gcloud compute config-ssh
 for i in $(ls $HOME/3D-Rot-me/*.blend); do
   j=$(basename $i .blend)
   while [ $next_frame -lt $end_frame ]; do
-    ssh $director_server 'echo "$(date) Blender is running..." \
-      >> $HOME/log.txt'
+    ssh $director_server "echo \"$(date) Blender is running...\" \
+      >> $HOME/log.txt"
     blender -b $HOME/3D-Rot-me/$j.blend -o $HOME/3D-Rot-$j/jpg/#.jpg -E CYCLES \
       -F JPG -s $next_frame -e $end_frame -a
     next_frame=$(basename $(ls -1 $HOME/3D-Rot-$j/jpg | sort -g | tail -1) \
       .jpg)
-    ssh $director_server 'echo "$(date) Blender stopped..." \
-      >> $HOME/log.txt'
+    ssh $director_server "echo \"$(date) Blender stopped...\" \
+      >> $HOME/log.txt"
     attempt=$((attempt+1))
     if [ $attempt -gt $limit ]; then
       ssh $director_server "echo '$(date) Something is not right. \
@@ -28,8 +28,8 @@ for i in $(ls $HOME/3D-Rot-me/*.blend); do
       break
     elif [ $next_frame -ge $end_frame ]; then
       ssh $director_server "echo \"$(date) Render successful.\" >> $HOME/log.txt"
-      ssh $director_server 'mv $HOME/being_rendered $HOME/rendered'
-      ssh $director_server 'mkdir -p $HOME/rendered_images/'
+      ssh $director_server "mv $HOME/being_rendered/$j.blend $HOME/rendered"
+      ssh $director_server "mkdir -p $HOME/rendered_images/"
       scp -r $HOME/3D-Rot-$j $director_server:rendered_images
       break
     else
