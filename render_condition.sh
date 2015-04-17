217fd8b5a5d3f1ded49eed978efe7c239870ecfe
Winston Wilkins <willo.jnr@gmail.com>
2015-04-17 04:29:22 -0500

    Changed the endframe parameter in render_condition.sh to 240

--- render_condition.sh
+++ render_condition.sh
@@ -2,7 +2,7 @@
 #replace with python. Script is over 20 lines.
 director_server=ubuntu@blender-director.us-central1-b.spartan-lacing-691
 next_frame=1
-end_frame=60
+end_frame=240
 attempt=0
 limit=2
 
