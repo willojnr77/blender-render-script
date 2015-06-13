cda6fc71a1dad229045ccf9f38876712abc1a786
Winston Wilkins <willo.jnr@gmail.com>
2015-06-12 22:40:53 -0500

    1933-2432

--- render_condition.sh
+++ render_condition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #replace with python. Script is over 20 lines.
 director_server=ubuntu@blender-director.us-central1-b.spartan-lacing-691
-next_frame=1
-end_frame=30
+next_frame=1933
+end_frame=2432
 attempt=0
 limit=2
 
